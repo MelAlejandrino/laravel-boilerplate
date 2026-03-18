bc0da9a5b6840ae3c93c72f08d75e168720751ce
MelAlejandrino <alejandrinomel02@gmail.com>
2026-03-19 00:51:06 +0800

    fix: console log

--- resources/js/hooks/use-permission.ts
+++ resources/js/hooks/use-permission.ts
@@ -3,13 +3,12 @@ import { useCallback } from 'react';
 
 export const usePermission = () => {
     const { auth } = usePage().props;
-    console.log('auth', auth);
 
     const hasPermission = useCallback(
         (permission: string) => {
             if (auth.roles.includes('super-admin')) {
-return true;
-}
+                return true;
+            }
 
             return auth.permissions.includes(permission);
         },
